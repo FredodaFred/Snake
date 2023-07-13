5af0183539029cfbd7ae9fdbdf55c23a5284401d
Frederick Sion <62347411+FredodaFred@users.noreply.github.com>
2023-07-13 15:28:14 -0400

    init commit

---
+++ PathAgent.js
@@ -0,0 +1,9 @@
+class PathAgent{
+    //Hamiltonian Cycle
+    //grid - Entire grid
+    //snake - snake array
+    //food - coordinate of destination
+    hamiltonianPath(grid, snake, food){
+
+    }
+}
